9d1e7036acb2a29c5f69f6db5a78f4a4899c835b
KFkas <48663613+1kfkas@users.noreply.github.com>
2024-04-17 22:42:31 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const listaAtributos = new Map();
+const listaAtributos = new Array();
 
-function addAtributo() {
+function addAtributo(){
   const caixaDeTexto = document.getElementById("set-atributo");
   
-  if(listaAtributos.has(caixaDeTexto.value) == false && caixaDeTexto.value != ""){
+  if(listaAtributos.includes(caixaDeTexto.value) == false && caixaDeTexto.value != ""){
     const newDiv = document.createElement("div");
 
     newDiv.classList.add("atributo-item");
@@ -12,7 +12,7 @@ function addAtributo() {
     const currentDiv = document.getElementById("atributos");
     newDiv.addEventListener("click", (e) => {
       let idSelf = e.target.id;
-      listaAtributos.delete(String(idSelf));
+      listaAtributos.splice(listaAtributos.indexOf(String(idSelf)), 1);
       currentDiv.removeChild(document.getElementById(idSelf));
     });
 
@@ -20,7 +20,7 @@ function addAtributo() {
     newDiv.appendChild(newContent);
 
     currentDiv.appendChild(newDiv);
-    listaAtributos.set(String(newDiv.id), String(caixaDeTexto.value));
+    listaAtributos.push(String(caixaDeTexto.value));
     caixaDeTexto.value = "";
   } else {
     $("#naopode-atributo").show();
@@ -28,18 +28,68 @@ function addAtributo() {
   }
 }
 
-/*
-document.getElementById("set-atributo").addEventListener("keypress", function(event){
-  console.log("Allou");
-  
-});
-*/
+function gerarCodigo(){
 
-$("#set-atributo").ready(function(){
-  document.getElementById("set-atributo").addEventListener("keypress", function(event){
-    if(event.key === "Enter"){
-      event.preventDefault();
-      addAtributo();
+  // Pega o elemento que mais vai ser usado : "caixa"
+
+  const caixaElement = document.getElementById("caixa");
+
+  // Remove todo o conteúdo da caixa
+
+  if(caixaElement.hasChildNodes()){
+    while(caixaElement.firstChild){
+      caixaElement.removeChild(caixaElement.firstChild);
     }
+  }
+
+  // Inicia o texto da classe
+
+  const classElement = document.getElementById("nome-classe");
+  const newClass = document.createElement("p");
+
+  newClass.innerText = "class "+classElement.value+":";
+  caixaElement.appendChild(newClass);
+
+  // Faz o __init__
+
+  let newP = "<p>&nbsp;&nbsp;&nbsp;&nbsp;def __init__(self";
+
+  listaAtributos.forEach((atributo) => {
+    newP += ", "+atributo
   });
-});
+  
+  newP += "):</p>";
+
+  caixaElement.insertAdjacentHTML('beforeend', newP);
+
+  listaAtributos.forEach((atributo) => {
+    const newInitP = "<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;self."+atributo+" = "+atributo+"</p>";
+
+    caixaElement.insertAdjacentHTML('beforeend', newInitP);
+  });
+
+  // Pega umas checkbox
+
+  const getBoxElement = document.getElementById("colocar-gets");
+  const setBoxElement = document.getElementById("colocar-sets");
+
+  if(getBoxElement.checked){
+    listaAtributos.forEach((atributo) => {
+      caixaElement.insertAdjacentHTML('beforeend', "<p></p>");
+
+      const capitalizedAtributo = atributo[0].toUpperCase()+atributo.slice(1);
+      const newGetP = "<p>&nbsp;&nbsp;&nbsp;&nbsp;def "+"get"+capitalizedAtributo+"(self):</p> <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return self."+atributo+"</p>";
+      caixaElement.insertAdjacentHTML('beforeend', newGetP);
+    });
+  }
+
+  if(setBoxElement.checked){
+    listaAtributos.forEach((atributo) => {
+      caixaElement.insertAdjacentHTML('beforeend', "<p></p>");
+
+      const capitalizedAtributo = atributo[0].toUpperCase()+atributo.slice(1);
+      const newGetP = "<p>&nbsp;&nbsp;&nbsp;&nbsp;def "+"set"+capitalizedAtributo+"(self, "+atributo+"):</p> <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;self."+atributo+" = "+atributo+"</p>";
+      caixaElement.insertAdjacentHTML('beforeend', newGetP);
+    });
+  }
+}
